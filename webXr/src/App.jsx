bdfd970ce7d80e8bbd7202a8e6419759a031ec0c
esayas188 <esayasfekade188@gmail.com>
2024-09-15 04:58:46 -0400

    feat: add three fiber

--- webXr/src/App.jsx
+++ webXr/src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { useRef } from 'react';
+import { Canvas,useFrame } from '@react-three/fiber';
+
+function RotatingSphere() {
+  const sphereRef = useRef();
+  
+  // useFrame can now be used inside this component
+  useFrame(() => {
+    if (sphereRef.current) {
+      sphereRef.current.rotation.x += 0.005;
+      sphereRef.current.rotation.y += 0.005;
+      sphereRef.current.rotation.z += 0.005;
+    }
+  });
+
+  return (
+    <mesh ref={sphereRef}>
+      <sphereGeometry args={[2, 30, 30]} />
+      <meshStandardMaterial color={0x00bfff} wireframe  />
+    </mesh>
+  );
+}
 
 export default function App() {
+
+
+
   return (
-    <div>App</div>
+    <div id='canvas-container'>
+      <Canvas camera={{ position: [2, 5, 1] }}>
+          <RotatingSphere/>
+
+          <directionalLight position={[2, 2, 2]} />
+      </Canvas>
+    </div>
   )
 }
